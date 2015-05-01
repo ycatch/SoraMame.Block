4ce72d389c14194dca17fb2057ea63a42c5ac8f5
Yutaka Catch <y-catch@ja2.so-net.ne.jp>
2015-05-01 11:43:00 +0900

    divide html to parts. add make script.
    make html files from some modules.

---
+++ dev/make_index.html.bat
@@ -0,0 +1,2 @@
+cd html_parts
+type head_common.html head_title_index.html head_links_common.html body_navbar_index.html body_pallette_index.html body_mainEditArea_index.html body_common_trashArea.html body_common_codeView.html body_common_expEditor.html body_common_help.html footer_common.html> ../../index.html
